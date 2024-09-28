6e51333908e27375185855eaea75e6575cbacb7d
Muhammad Faizan <muhammadfaizan@Muhammads-MacBook-Pro.local>
2024-09-28 05:20:18 -0800

    add updated code

--- src/features/home/homeHeader.js
+++ src/features/home/homeHeader.js
@@ -239,7 +239,9 @@ const HomeHeader = () => {
                 </div>
             </header>
             <main>
-               
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                </div>
             </main>
         </div>
     );
